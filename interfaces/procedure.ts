e52f33600da1bf4d21e9a689c85188d0799917cf
cesaraugustomt <contatocsaugusto@gmail.com>
2023-09-05 09:15:17 -0300

    adiciona interface `procedure`

---
+++ interfaces/procedure.ts
@@ -0,0 +1,13 @@
+export interface IProcedure {
+  id: string;
+  code: string;
+  name: string;
+  description: string;
+  status: boolean;
+  initial_competence: string;
+
+  createdAt: string;
+  updatedAt: string;
+}
+
+export type IDataProcedure = 'sdasd' | 'sfdasd';
